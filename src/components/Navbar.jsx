5ff7e3166bfc02cfc032df5c8b24621657bfb336
iftekhar ansari <ansariiftekhar523@gmail.com>
2025-08-13 16:58:49 +0530

    mobile sidebar closing issue solved etc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
         {!loading && (
           <div className="flex items-center gap-1 md:gap-4 cursor-pointer">
             {/* Theme Switch */}
-            <div className="flex items-center gap-2 px-3 py-2 rounded-full bg-gray-100 dark:bg-gray-800">
+            <div className="flex items-center gap-2 px-3  py-2 rounded-full bg-gray-100 dark:bg-gray-800">
               {theme === "light" && <Sun size={16} />}
               {theme === "dark" && <Moon size={16} />}
               {theme === "system" && <Laptop size={16} />}
@@ -76,13 +76,13 @@ export default function Navbar() {
             {!user ? (
               <>
                 <Link href="/sign-in" className="">
-                  <Button variant="outline" className="cursor-pointer p-2">
+                  <Button variant="outline" className="cursor-pointer ">
 
                     Sign In
                   </Button>
                 </Link>
                 <Link href="/sign-up">
-                  <Button className="cursor-pointer bg-indigo-600 text-white shadow-lg p-2">
+                  <Button className="cursor-pointer bg-indigo-600 text-white shadow-lg ">
                     Sign Up
                   </Button>
                 </Link>
